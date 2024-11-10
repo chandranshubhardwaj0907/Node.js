4149bc737ce70f26fc951eb7d0be93b3df938858
chandranshu bhardwaj <chandranshubhardwaj68@gmail.com>
2024-11-10 15:31:06 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,39 @@
-oMZU6gz9uKBz0Xkl
 
-const URI = "mongodb+srv://chandranshubhardwaj:oMZU6gz9uKBz0Xkl@movierecommendation.xdaqs.mongodb.net/"
+
+const { MongoClient } = require('mongodb');
+
+
+
+const url = 'mongodb+srv://chandranshubhardwaj:<Password>@movierecommendation.xdaqs.mongodb.net/';
+const client = new MongoClient(url);
+
+const dbName = 'HelloWorld';
+
+async function main() {
+  
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+
+// insert
+  const data = {
+    "First_name" : "Ashmit",
+    "Last_name" : "Thawait",
+    "City" : "Raigarh",
+    "Phone_number": "7987403728"
+  };
+  const insertResult = await collection.insertMany([data]);
+console.log('Inserted documents =>', insertResult);
+
+ // Read
+  const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
